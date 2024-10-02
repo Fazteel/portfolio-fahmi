a378cfe56a59a54880ebe2bee94b0058ddde8cba
Fazteel <100552801+Fazteel@users.noreply.github.com>
2024-10-03 01:07:59 +0700

    Penambahan Fitur

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@
 import React from 'react';
 import Navbar from './layout/Navbar';
 import Footer from './layout/Footer';
-import Home from './views/Home';
+import Hero from './views/Hero';
+import About from './views/About';
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className="flex flex-col">
+        <Navbar />
+        <main className="flex-grow text-center bg-gray-100 dark:bg-gray-800">
+          <Hero />
+          <div className="mt-28">
+            <About />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/DarkModeToggle.jsx
@@ -0,0 +1,50 @@
+// src/components/DarkModeToggle.jsx
+import React, { useEffect, useState } from 'react';
+
+const DarkModeToggle = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      setDarkMode(true);
+      document.documentElement.classList.add('dark');
+    } else {
+      setDarkMode(false);
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (darkMode) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <button
+      onClick={toggleDarkMode}
+      className="text-black dark:text-white p-2 rounded flex items-center justify-center"
+      aria-label="Toggle Dark Mode"
+    >
+      {darkMode ? (
+        // Ikon untuk dark mode (bulan)
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fillRule="evenodd" d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z" clipRule="evenodd" />
+        </svg>
+      ) : (
+        // Ikon untuk light mode (matahari)
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <path fillRule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clipRule="evenodd" />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+export default DarkModeToggle;
